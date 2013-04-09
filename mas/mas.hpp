4c96d8801b4b0521ea6b6c1f9e433db44e2624cf
Joseph Noir <joseph.noir@gmail.com>
2013-04-09 08:45:03 +0200

    added agent and evironment

---
+++ mas/mas.hpp
@@ -0,0 +1,6 @@
+#ifndef MAS_HPP
+#define MAS_HPP
+
+#include "cppa/cppa.hpp"
+
+#endif
--- src/mas.cpp
+++ src/mas.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+#include "cppa/cppa.hpp"
+
+#include "mas/agent.hpp"
+#include "mas/environment.hpp"
+
+using namespace std;
+using namespace cppa;
 
 
 auto main () -> int{
+
+    auto env = spawn<environment>();
+
+
+    await_all_others_done();
+    shutdown();
     return EXIT_SUCCESS;
 }
